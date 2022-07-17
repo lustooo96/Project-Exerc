450d7a42f1fdc36d6c53d83c527071c95f61e2ce
lustooo96 <91395552+lustooo96@users.noreply.github.com>
2022-07-17 18:07:55 -0300

    Create exercicio-1-3.js

---
+++ exercicio-1-3.js
@@ -0,0 +1,29 @@
+
+console.log(teste('999'));
+
+function teste(number){
+  if(!number) return 'Passe uma string para validar'
+  if(validString(number)){
+     //retira todos os espaços da string que vai se transformar em numero
+     let numberWithoutSpace =  parseInt(number.replace(/\s+/g, ''));  
+    	//Valida um inteiro safe
+    	if(Number.isSafeInteger(numberWithoutSpace)) return numberWithoutSpace
+      else return 'Numero Inválido'
+        
+  }else {
+    return 'Numero Inválido'
+  }
+}
+
+function validString(number){
+
+  ////retorna false se tiver letra(invalida a função)
+  if(number.match(/[a-zA-Z,.]/g)) return false
+  
+  for(let i=0 ; i < number.length ; i++){
+    //Validando se não tem mais de um sinal de + ou de -
+    if(i > 1 && (number[i] === '+' || number[i] === '-')) return false
+
+  }
+  return true;
+}
